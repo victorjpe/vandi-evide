0663bac5d61eb83f47268ff8fddba6080dbcece3
victorjpe <victor.jose@logicline.de>
2017-07-11 15:31:51 +0530

    Updating contribute page

--- src/pages/contribute/contribute.ts
+++ src/pages/contribute/contribute.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 export class ContributePage {
 	rating : number = 0;
 	stars : any[] = [0, 1, 2, 3, 4];
+
+	updateRating(index) {
+		this.rating = index+1;
+	}
 }
